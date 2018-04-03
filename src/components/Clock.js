0c620d3b3913a8a26c5886d0915523fc018aef6e
neilmyers <mmyersneil@gmail.com>
2018-04-03 11:54:38 -0600

    added js timer

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -5,15 +5,43 @@ class Clock extends Component {
   constructor(props) {
     super(props)
 
+    this.timer = 0;
+    this.birthday = props.birthdayFormState.startDate.toString();
+    this.getTimeRemaining = this.getTimeRemaining.bind(this);
 
     this.state = {
-      timeRemaining: {}
+      timeRemaining: this.getTimeRemaining(props.birthdayFormState.startDate.toString())
     }
+  }
+
+  getTimeRemaining(birthday) {
+    var bday = new Date(birthday);
+    let today = new Date();
+
+    var distance = bday.getTime() - today.getTime();
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (100 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    return {
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    }
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      const timeRemaining = this.getTimeRemaining(this.birthday)
+      this.setState({ timeRemaining: timeRemaining })
+    }, 1000);
   }
 
   render() {
     const data = this.state.timeRemaining
-
     return (
       <div>
         <div>DAYS {data.days}</div>
@@ -22,11 +50,6 @@ class Clock extends Component {
         <div>SECS {data.seconds}</div>
       </div>
     )
-  }
-
-  componentDidMount() {
-    this.birthday = this.props.birthdayFormState.startDate.toString();
-    alert(this.birthday);
   }
 }
 
