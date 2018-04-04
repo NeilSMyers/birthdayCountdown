256a0f2c782e1bfb7be48652ce1419f9bd9629f9
neilmyers <mmyersneil@gmail.com>
2018-04-04 16:21:04 -0600

    added countdown clock skew

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,13 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <header> 
+          <h1>Birthday Countdown</h1>
+          <div className="header__skew">
+            <div className="header__subskew">
+            </div>
+          </div>
+        </header>
         <BirthdayForm/>
       </div>
     );
